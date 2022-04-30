876ce03d5952f60a9a6a781f6520c399b5e4f11d
Stanislav Kosáček <stanislav.kosacek@icloud.com>
2022-04-30 13:54:35 +0200

    cviceni 1-4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+});
+
+const closeMenu = () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.add('nav-closed');
+};
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', closeMenu);
+});
+
+let ordered = false;
+const orderBtnElm = document.querySelector('.order-btn');
+orderBtnElm.addEventListener('click', () => {
+  const drinkCupElm = document.querySelector('.drink__cup');
+  if (!ordered) {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderBtnElm.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
